23a8aa66a64cb202c08476220a80bc09738cca6f
1vanytska <sofiia.ivanytska.22@pnu.edu.ua>
2024-10-25 13:10:11 +0300

    Add homepage

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask  # From module flask import class Flask
-app = Flask(__name__)    # Construct an instance of Flask class for our webapp
+from flask import Flask, request, redirect
+app = Flask(__name__)    
 
-@app.route('/')          # URL '/' to be handled by main() route handler
+@app.route('/')   # URL '/' to be handled by main() route handler
 def main():
     return 'Hello, world!'
 
-if __name__ == '__main__':
-    app.run(debug=True)  # Launch built-in web server and run this Flask webapp 
+@app.route('/homepage')   # URL '/' to be handled by main() route handler
+def home():
+    """View for the Home page of your website."""
+    return "This is your Home page :)"
+
+if __name__ == "__main__":
+    app.run()  # Launch built-in web server and run this Flask webapp, debug=True
+ 
+
