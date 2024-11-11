bd70bc6b06fddb57f0a274ef9dfc4e7ba9085ac4
1vanytska <sofiia.ivanytska.22@pnu.edu.ua>
2024-11-11 20:36:29 +0200

    Add cookie

--- app/users/views.py
+++ app/users/views.py
@@ -1,5 +1,6 @@
 from . import user_bp
-from flask import redirect, render_template, abort, request, url_for
+from flask import make_response, redirect, render_template, request, url_for
+from datetime import datetime, timedelta
 
 @user_bp.route('/hi/<string:name>') # --> user/hi/sofiia?age=19
 def greetings(name):
@@ -12,3 +13,21 @@ def admin():
     to_url = url_for("users.greetings", name="administrator", _external=True)    # --> "http://localhost:8080/user/hi/administrator"
     print(to_url)
     return redirect(to_url)
+
+@user_bp.route('/set_cookie')
+def set_cookie():
+    response = make_response('Кука встановлена')
+    response.set_cookie('username', 'student', max_age=timedelta(seconds=60))
+    response.set_cookie('color', '', max_age=timedelta(seconds=60))
+    return response
+
+@user_bp.route('/get_cookie')
+def get_cookie():
+    username = request.cookies.get('username')
+    return request.cookies
+
+@user_bp.route('/delete_cookie')
+def delete_cookie():
+    response = make_response('Кука видалена')
+    response.set_cookie('username', '', expires=0) # response.set_cookie('username', '', max_age=0)
+    return response
