5f4cad2ee16cfbf871706e5589d97e02821a4a0e
1vanytska <sofiia.ivanytska.22@pnu.edu.ua>
2024-11-17 20:40:31 +0200

    Add simple json and styles

--- app/posts/forms.py
+++ app/posts/forms.py
@@ -1,16 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, DateField, SelectField, StringField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
 
-CATEGORIES = [('tech', 'Tech'), ('science', 'Science'), ('lifestyle', 'Lifestyle')]
-
 class PostForm(FlaskForm):
-    title = StringField("Title", validators=[DataRequired(), Length(min=2)])
-    content = TextAreaField("Content", render_kw={"rows": 5, "cols": 40}, validators=[DataRequired()])
-    is_active = BooleanField('Active Post')
-    publish_date = DateField('Publish Date',
-                             format='%Y-%m-%d', validators=[DataRequired()])
-    category = SelectField('Category',
-                           choices=CATEGORIES, validators=[DataRequired()])
-    submit =SubmitField("Add Post")
-
+    title = StringField("Title", 
+                        validators=[DataRequired(), Length(min=2, max=10)])
+    content = TextAreaField("Content", validators=[DataRequired()])
+    submit = SubmitField("Add Post")
--- app/posts/views.py
+++ app/posts/views.py
@@ -1,33 +1,33 @@
 from . import post_bp
-from flask import render_template, abort, flash, redirect, url_for
+from flask import render_template, abort, flash, url_for, redirect, request
 from .forms import PostForm
+from .utils import load_posts, save_post, get_post
 
-posts = [
-    {"id": 1, 'title': 'My First Post', 'content': 'This is the content of my first post.', 'author': 'John Doe'},
-    {"id": 2, 'title': 'Another Day', 'content': 'Today I learned about Flask macros.', 'author': 'Jane Smith'},
-    {"id": 3, 'title': 'Flask and Jinja2', 'content': 'Jinja2 is powerful for templating.', 'author': 'Mike Lee'}
-]
 
-@post_bp.route('/add_post', methods=['GET', 'POST'])
+@post_bp.route('/add_post', methods=["GET", "POST"]) 
 def add_post():
     form = PostForm()
     if form.validate_on_submit():
         title = form.title.data
         content = form.content.data
-        # Here you can add logic to save the post
-        print(title, content)
-        flash(f'Post {title} added successfully!', 'success')
-        return redirect(url_for('add_post'))
-    
-    return render_template("posts/add_post.html", form=form)
+        
+        new_post = {"id": len(load_posts()) + 1, 'title': title, 'content': content}
+        save_post(new_post)
+        flash(f"Post {title} added successfully!", "success")
+        return redirect(url_for(".get_posts"))
+    elif request.method == "POST":
+        flash(f"Enter the correct data in the form!", "danger")
+    return render_template("add_post.html", form=form)
 
-@post_bp.route('/')
+@post_bp.route('/') 
 def get_posts():
-    return render_template('posts/posts.html', posts=posts)
+    posts = load_posts()
+    return render_template("posts.html", posts=posts)
 
-@post_bp.route('/<int:id>')
+@post_bp.route('/<int:id>') 
 def detail_post(id):
-    if id > 3:
-        abort(404)
-    post = posts[id - 1]
-    return render_template('posts/detail_post.html', post=post)
+    post = get_post(id)
+    if not post:
+        return abort(404)
+    return render_template("detail_post.html", post=post)
+    
