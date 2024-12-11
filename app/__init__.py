568f84abfb1c200d2f6c6d85c636736c8fcfbc13
1vanytska <sofiia.ivanytska.22@pnu.edu.ua>
2024-12-11 21:22:19 +0200

    Add register, log in, logout, account and users (without style) - Task 1-4 (Part 1)

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,22 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy.orm import DeclarativeBase
+from flask_bcrypt import Bcrypt
 
 class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
+bcrypt = Bcrypt()
 
 def create_app(config_name="config"):
     app = Flask(__name__)
-    app.config.from_object(config_name) #налаштування з об'єкта
+    app.config.from_object(config_name)
+
     db.init_app(app)
     migrate.init_app(app, db)
+    bcrypt.init_app(app)
 
     with app.app_context():
         from . import views
