4bbb821a0c578239c535bac40db7f478399108f4
1vanytska <sofiia.ivanytska.22@pnu.edu.ua>
2024-12-18 20:23:47 +0200

    Add profile image, migration and styles - Task 1-2

--- app/users/__init__.py
+++ app/users/__init__.py
@@ -1,6 +1,10 @@
 from flask import Blueprint
 
-user_bp = Blueprint("users", __name__, url_prefix="/user",
-                    template_folder="templates/users")
+user_bp = Blueprint("users", 
+                    __name__, 
+                    url_prefix="/user",
+                    template_folder="templates/users",
+                    static_folder="static",
+                    static_url_path="/static_for_users")
 
 from . import views
--- app/users/models.py
+++ app/users/models.py
@@ -8,6 +8,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    image_file = db.Column(db.String(20), nullable=True, default='default.jpg')
 
     def __repr__(self):
         return f"User('{self.email}')"
