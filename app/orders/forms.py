25a148b5c2b65ec91a36ca61faf6860039de5312
1vanytska <sofiia.ivanytska.22@pnu.edu.ua>
2024-12-19 13:57:12 +0200

    Add list of orders, filters and sorting - Task 6

--- app/orders/forms.py
+++ app/orders/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, FloatField, SelectMultipleField, SubmitField
+from wtforms import SelectField, StringField, FloatField, SelectMultipleField, SubmitField
 from wtforms.validators import DataRequired, Length, NumberRange
+from app.orders.models import Category
 
 class CreateOrderForm(FlaskForm):
     name = StringField(
@@ -21,3 +22,23 @@ class CreateOrderForm(FlaskForm):
         coerce=int
     )
     submit = SubmitField('Create Order')
+
+class SearchForm(FlaskForm):
+    search_field = StringField('Search', validators=[DataRequired()])
+    search_by = SelectField('Search by', choices=[
+        ('name', 'Name'),
+        ('comment', 'Comment'),
+        ('price', 'Price')
+    ], validators=[DataRequired()])
+    category_id = SelectField('Category', choices=[
+        (category.id, category.name) for category in Category.query.all()
+    ])
+    ignore_category_filter = SelectField('Ignore Category', choices=[
+        ('yes', 'Yes'), 
+        ('no', 'No')
+    ], default='no')
+    sort_by = SelectField('Sort by', choices=[
+        ('name', 'Name'),
+        ('price', 'Price')
+    ], validators=[DataRequired()])
+    submit = SubmitField('Search')
--- app/orders/views.py
+++ app/orders/views.py
@@ -2,12 +2,28 @@ from . import order_bp
 from flask import render_template, redirect, url_for, flash
 from app import db
 from app.orders.models import Order, Category
-from .forms import CreateOrderForm
+from .forms import CreateOrderForm, SearchForm
 
-@order_bp.route('/orders', methods=['GET'])
+@order_bp.route('/orders', methods=['GET', 'POST'])
 def orders():
-    all_orders = Order.query.all()
-    return render_template('orders.html', orders=all_orders)
+    form = SearchForm()
+
+    orders_query = Order.query
+
+    if form.ignore_category_filter.data == 'no' and form.category_id.data:
+        orders_query = orders_query.join(Order.categories).filter(Category.id == form.category_id.data)
+
+    if form.search_field.data:
+        search_field = form.search_by.data
+        search_value = form.search_field.data
+        orders_query = orders_query.filter(getattr(Order, search_field).like(f'%{search_value}%'))
+
+    sort_by = form.sort_by.data
+    orders_query = orders_query.order_by(getattr(Order, sort_by))
+
+    orders = orders_query.all()
+
+    return render_template('orders.html', form=form, orders=orders)
 
 @order_bp.route('/create_order', methods=['GET', 'POST'])
 def create_order():
